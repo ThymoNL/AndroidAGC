503d939cbfc16723ee2fc0a3cf98f7d7282c3eb1
Thymo van Beers <thymovanbeers@gmail.com>
2018-04-28 01:24:38 +0200

    Move IndicatorPanel.init() up

--- app/src/main/java/nl/thymo/virtualagc/control/IndicatorPanel.java
+++ app/src/main/java/nl/thymo/virtualagc/control/IndicatorPanel.java
@@ -43,6 +43,43 @@ public class IndicatorPanel extends LinearLayout {
         init();
     }
 
+    private void init() {
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.indicators, this);
+
+        uplinkLight = findViewById(R.id.uplinkLight);
+        attLight = findViewById(R.id.attLight);
+        stbyLight = findViewById(R.id.stbyLight);
+        keyrelLight = findViewById(R.id.keyrelLight);
+        oprerrLight = findViewById(R.id.oprerrLight);
+        priodispLight = findViewById(R.id.priodispLight);
+        nodapLight = findViewById(R.id.nodapLight);
+        tempLight = findViewById(R.id.tempLight);
+        gimballockLight = findViewById(R.id.gimballockLight);
+        progLight = findViewById(R.id.progLight);
+        restartLight = findViewById(R.id.restartLight);
+        trackerLight = findViewById(R.id.trackerLight);
+        altLight = findViewById(R.id.altLight);
+        velLight = findViewById(R.id.velLight);
+
+        // Set resources to use for indicators
+        uplinkLight.setResources(R.mipmap.uplinkactyon, R.mipmap.uplinkactyoff);
+        attLight.setResources(R.mipmap.noatton, R.mipmap.noattoff);
+        stbyLight.setResources(R.mipmap.stbyon, R.mipmap.stbyoff);
+        keyrelLight.setResources(R.mipmap.keyrelon, R.mipmap.keyreloff);
+        oprerrLight.setResources(R.mipmap.oprerron, R.mipmap.oprerroff);
+        priodispLight.setResources(R.mipmap.priodispon, R.mipmap.priodispoff);
+        nodapLight.setResources(R.mipmap.nodapon, R.mipmap.nodapoff);
+        tempLight.setResources(R.mipmap.tempon, R.mipmap.tempoff);
+        gimballockLight.setResources(R.mipmap.gimballockon, R.mipmap.gimballockoff);
+        progLight.setResources(R.mipmap.progon, R.mipmap.progoff);
+        restartLight.setResources(R.mipmap.restarton, R.mipmap.restartoff);
+        trackerLight.setResources(R.mipmap.trackeron, R.mipmap.trackeroff);
+        altLight.setResources(R.mipmap.alton, R.mipmap.altoff);
+        velLight.setResources(R.mipmap.velon, R.mipmap.veloff);
+    }
+
     public void turnOn(Indicator light) {
         setState(light, true);
     }
@@ -96,42 +133,5 @@ public class IndicatorPanel extends LinearLayout {
                 velLight.setState(state);
                 break;
         }
-    }
-
-    private void init() {
-        LayoutInflater inflater = (LayoutInflater) getContext()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.indicators, this);
-
-        uplinkLight = findViewById(R.id.uplinkLight);
-        attLight = findViewById(R.id.attLight);
-        stbyLight = findViewById(R.id.stbyLight);
-        keyrelLight = findViewById(R.id.keyrelLight);
-        oprerrLight = findViewById(R.id.oprerrLight);
-        priodispLight = findViewById(R.id.priodispLight);
-        nodapLight = findViewById(R.id.nodapLight);
-        tempLight = findViewById(R.id.tempLight);
-        gimballockLight = findViewById(R.id.gimballockLight);
-        progLight = findViewById(R.id.progLight);
-        restartLight = findViewById(R.id.restartLight);
-        trackerLight = findViewById(R.id.trackerLight);
-        altLight = findViewById(R.id.altLight);
-        velLight = findViewById(R.id.velLight);
-
-        // Set resources to use for indicators
-        uplinkLight.setResources(R.mipmap.uplinkactyon, R.mipmap.uplinkactyoff);
-        attLight.setResources(R.mipmap.noatton, R.mipmap.noattoff);
-        stbyLight.setResources(R.mipmap.stbyon, R.mipmap.stbyoff);
-        keyrelLight.setResources(R.mipmap.keyrelon, R.mipmap.keyreloff);
-        oprerrLight.setResources(R.mipmap.oprerron, R.mipmap.oprerroff);
-        priodispLight.setResources(R.mipmap.priodispon, R.mipmap.priodispoff);
-        nodapLight.setResources(R.mipmap.nodapon, R.mipmap.nodapoff);
-        tempLight.setResources(R.mipmap.tempon, R.mipmap.tempoff);
-        gimballockLight.setResources(R.mipmap.gimballockon, R.mipmap.gimballockoff);
-        progLight.setResources(R.mipmap.progon, R.mipmap.progoff);
-        restartLight.setResources(R.mipmap.restarton, R.mipmap.restartoff);
-        trackerLight.setResources(R.mipmap.trackeron, R.mipmap.trackeroff);
-        altLight.setResources(R.mipmap.alton, R.mipmap.altoff);
-        velLight.setResources(R.mipmap.velon, R.mipmap.veloff);
     }
 }

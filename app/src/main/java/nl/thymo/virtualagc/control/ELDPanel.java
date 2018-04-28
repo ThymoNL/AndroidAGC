0f9501843ed3e113c9b3c1bc882744a93055d727
Thymo van Beers <thymovanbeers@gmail.com>
2018-04-28 23:13:28 +0200

    Add state swichting to register separators

--- app/src/main/java/nl/thymo/virtualagc/control/ELDPanel.java
+++ app/src/main/java/nl/thymo/virtualagc/control/ELDPanel.java
@@ -13,12 +13,12 @@ import nl.thymo.virtualagc.R;
 
 public class ELDPanel extends ConstraintLayout {
     //TODO: Convert horizontal separators to IndicatorLight (for P06)
-    private IndicatorLight eldActy, eldProg, eldVerb, eldNoun;
+    private IndicatorLight eldActy, eldProg, eldVerb, eldNoun, r1Sep, r2Sep, r3Sep;
     private ELDPair progPair, verbPair, nounPair;
     private ELDRow r1Eld, r2Eld, r3Eld;
 
     public enum ELDIndicator {
-        ACTY, PROG, VERB, NOUN
+        ACTY, PROG, VERB, NOUN, R1SEP, R2SEP, R3SEP
     }
 
     public enum ELDDigitRow {
@@ -55,12 +55,18 @@ public class ELDPanel extends ConstraintLayout {
         r1Eld = findViewById(R.id.r1Eld);
         r2Eld = findViewById(R.id.r2Eld);
         r3Eld = findViewById(R.id.r3Eld);
+        r1Sep = findViewById(R.id.r1Sep);
+        r2Sep = findViewById(R.id.r2Sep);
+        r3Sep = findViewById(R.id.r3Sep);
 
         // Set resources for indicators
         eldActy.setResources(R.mipmap.compactyon, R.mipmap.compactyoff);
         eldProg.setResources(R.mipmap.rprogon, R.mipmap.rprogoff);
         eldVerb.setResources(R.mipmap.verbon, R.mipmap.verboff);
         eldNoun.setResources(R.mipmap.nounon, R.mipmap.nounoff);
+        r1Sep.setResources(R.mipmap.hseparatoron, R.mipmap.hseparator);
+        r2Sep.setResources(R.mipmap.hseparatoron, R.mipmap.hseparator);
+        r3Sep.setResources(R.mipmap.hseparatoron, R.mipmap.hseparator);
     }
 
     public void turnOn(ELDIndicator light) {
@@ -84,6 +90,15 @@ public class ELDPanel extends ConstraintLayout {
                 break;
             case NOUN:
                 eldNoun.setState(state);
+                break;
+            case R1SEP:
+                r1Sep.setState(state);
+                break;
+            case R2SEP:
+                r2Sep.setState(state);
+                break;
+            case R3SEP:
+                r3Sep.setState(state);
                 break;
         }
     }
